30b2e0bd0d832a160dfc7cd8983b157f9a7815f6
fatur-bbx <fatur21ti@mahasiswa.pcr.ac.id>
2023-06-15 10:56:23 +0700

    Templating

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+
+const routes = [
+  {
+    path : '/',
+    name : 'post.index',
+    component: () => import('@/views/post/Index.vue')
+  },
+  {
+    path : '/create',
+    name : 'post.create',
+    component : () => import('@/views/post/Create.vue')
+  },
+  {
+    path : '/edit/:id',
+    name : 'post.edit',
+    component : () => import('@/views/post/Edit.vue')
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
